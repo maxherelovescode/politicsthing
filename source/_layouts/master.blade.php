a5fd24a03d4723c9473d0ffdad13b3f29a673740
Max H <maxplaysbest@gmail.com>
2021-08-16 14:24:18 +1200

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav>
-                <img src='https://res.cloudinary.com/maxsamazingsite/image/upload/v1629080474/politics%20for%20school/image1_q62eqa.png'>
+                <img src='http://res.cloudinary.com/maxsamazingsite/image/upload/c_scale,w_32/v1629080474/politics%20for%20school/image1_q62eqa.png'>
                 <strong>{{ $page->site->title }}</strong><br>
                 <ul>
                     <li><a href="/">Home</a></li>
