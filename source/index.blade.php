5ba567e281101977ab5c61df5666ee21ac55935a
Max H <maxplaysbest@gmail.com>
2021-08-16 14:12:51 +1200

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,21 +1,10 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
+    <h1>Welcome to the future of politics</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
-
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    <p>By people for people our policies of education and health are going to help millions.</p>
+    <p>See our bills and policies on the posts page</p>
+    <p> Submit a bill on the contact page</p>
+    <p> Website by Max H </p>
 @endsection
